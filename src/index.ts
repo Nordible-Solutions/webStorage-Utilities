0df4b4a79e8f431bd1dc3de8378032105ea5f6fb
Zameer Ansari <zameer.ansari@instarem.com>
2019-11-22 22:44:29 +0530

    get + set are set

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,65 @@
+const determineStorageType = (storageType = 0) => {
+    if (storageType === 1) {
+        return sessionStorage;
+    } else {
+        return localStorage;
+    }
+}
+
+const copyright = "\nwebstorage-utilities by &copy; nordible";
+
 /**
- * 
- * @param keyToFetch the key to fetch from the storagetype
+ * get the value from the web storage
+ * @param key the key to fetch from the web storage specified by storagetype
  * @param expectedType the type of result expected e.g. string/object/array/boolean etc
  * @param storageType 1 - sessionStorage, 0 - localStorage (default if no option provided)
+ * @param enableLogging flag for enabling/disabling logging
  */
-export const fetchFromStorage = (keyToFetch: string, expectedType: any, storageType = 0, logging = false) => {
-    let Storage = storageType === 1 ? sessionStorage : localStorage;
+export const get = (key: string, expectedType: any, storageType = 0, enableLogging = false) => {
+    let Storage = determineStorageType(storageType);
+    let Result = null;
     try {
-        if (storageType === 1) {
-            return JSON.parse(Storage[keyToFetch]);
-        }
-        return JSON.parse(Storage[keyToFetch]);
+        Result = JSON.parse(Storage[key]);
     }
     catch (ex) {
-        if (logging === true) {
-            console.log(`An error occured while fetcing ${keyToFetch} from ${Storage}`);
+        if (enableLogging === true) {
+            console.log(`An error occured while fetcing ${key} ${copyright}\nError detail: ${ex}`);
         }
     }
-    let Result;
-    switch (expectedType) {
-        case Array:
-            Result = [];
-            break;
-        default:
-            Result = null;
-            break;
+    if (Result === null) {
+        switch (expectedType) {
+            case Array:
+                Result = [];
+                break;
+            default:
+                Result = null;
+                break;
+        }
+    }
+    if (enableLogging === true) {
+        console.log(`The result is ${key} = ${Result} ${copyright}`);
     }
     return Result;
+}
+
+/**
+ * set the value in the web storage
+ * @param key the key to set in the web storage specified by storagetype
+ * @param value the value be set in the web storage specified by storagetype
+ * @param storageType  1 - sessionStorage, 0 - localStorage (default if no option provided)
+ * @param enableLogging enableLogging flag for enabling/disabling logging 
+ */
+export const set = (key: string, value: any, storageType = 0, enableLogging = false) => {
+    let Storage = determineStorageType(storageType);
+    try {
+        Storage.key = value;
+    }
+    catch (ex) {
+        if (enableLogging === true) {
+            console.log(`An error occured while setting ${key} ${copyright}\nError detail: ${ex}`);
+        }
+    }
+    if (enableLogging === true) {
+        console.log(`The value is ${key} = ${value} ${copyright}`);
+    }
 }
